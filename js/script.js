3e9afdf389434ab2b2b05ea81283f16a9e7a6ea7
Mazengabr <154664065+Mazengabr@users.noreply.github.com>
2023-12-24 23:23:08 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navber');
+
+    menu.addEventListener('click', function () {
+        menu.classList.toggle('bx-x');
+        navbar.classList.toggle('open');
+    });
+
+    // إغلاق القائمة عند بدء تحميل الصفحة
+    navbar.classList.remove('open');
+
+    // إضافة رمز لإغلاق القائمة عند النقر على رابط داخل القائمة
+    document.querySelectorAll('.navber a').forEach(function (link) {
+        link.addEventListener('click', function () {
+            menu.classList.remove('.bx-x');
+            navbar.classList.remove('.open');
+        });
+    });
+});
+
+
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration: 2500,
+    reset: true
+});
+
+sr.reveal('.logo',{delay:200, origin: 'lift'});
+sr.reveal('.navber',{delay:400, origin: 'top'});
+sr.reveal('.main-btn',{delay:520, origin: 'right'});
+
+sr.reveal('.home-text span',{delay:600, origin: 'right'});
+sr.reveal('.home-text h1',{delay:680, origin: 'left'});
+sr.reveal('.home-text p',{delay:750, origin: 'right'});
+sr.reveal('.article-text p',{delay:750, origin: 'right'});
+sr.reveal('.main-btn',{delay:860, origin: 'lift'});
+
+sr.reveal('.share',{delay:950, origin: 'bottom'});
+
+sr.reveal('.home-img',{delay:1000, origin: 'right'});
